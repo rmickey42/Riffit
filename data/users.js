6bb91c6e36b680a3257688afe3fee0d9908f4f24
AlanAtrach1 <aatrach@stevens.edu>
2024-11-29 22:27:32 -0500

    users basic data methods

--- data/users.js
+++ data/users.js
@@ -8,36 +8,36 @@ let exportedMethods = {
     const userList = await userCollection.find({}).toArray();
     return userList;
   },
-  // async getUserById(id) {
-  //   id = validation.checkId(id);
-  //   const userCollection = await users();
-  //   const user = await userCollection.findOne({_id: new ObjectId(id)});
-  //   if (!user) throw 'Error: User not found';
-  //   return user;
-  // },
-  // async addUser(firstName, lastName) {
-  //   firstName = validation.checkString(firstName, 'First name');
-  //   lastName = validation.checkString(lastName, 'Last name');
+  async getUserById(id) {
+    id = validation.checkId(id);
+    const userCollection = await users();
+    const user = await userCollection.findOne({_id: new ObjectId(id)});
+    if (!user) throw 'Error: User not found';
+    return user;
+  },
+  async addUser(username, password) {
+    firstName = validation.checkString(firstName, 'First name');
+    lastName = validation.checkString(lastName, 'Last name');
 
-  //   let newUser = {
-  //     firstName: firstName,
-  //     lastName: lastName
-  //   };
-  //   const userCollection = await users();
-  //   const newInsertInformation = await userCollection.insertOne(newUser);
-  //   if (!newInsertInformation.insertedId) throw 'Insert failed!';
-  //   return await this.getUserById(newInsertInformation.insertedId.toString());
-  // },
-  // async removeUser(id) {
-  //   id = validation.checkId(id);
-  //   const userCollection = await users();
-  //   const deletionInfo = await userCollection.findOneAndDelete({
-  //     _id: new ObjectId(id)
-  //   });
-  //   if (!deletionInfo) throw `Error: Could not delete user with id of ${id}`;
+    let newUser = {
+      username,
+      password
+    };
+    const userCollection = await users();
+    const newInsertInformation = await userCollection.insertOne(newUser);
+    if (!newInsertInformation.insertedId) throw 'Insert failed!';
+    return await this.getUserById(newInsertInformation.insertedId.toString());
+  },
+  async removeUser(id) {
+    id = validation.checkId(id);
+    const userCollection = await users();
+    const deletionInfo = await userCollection.findOneAndDelete({
+      _id: new ObjectId(id)
+    });
+    if (!deletionInfo) throw `Error: Could not delete user with id of ${id}`;
 
-  //   return {...deletionInfo, deleted: true};
-  // },
+    return {...deletionInfo, deleted: true};
+  },
   // async updateUserPut(id, firstName, lastName) {
   //   id = validation.checkId(id);
   //   firstName = validation.checkString(firstName, 'first name');
@@ -59,30 +59,30 @@ let exportedMethods = {
   //   return updateInfo;
   // },
 
-  // async updateUserPatch(id, userInfo) {
-  //   id = validation.checkId(id);
-  //   if (userInfo.firstName)
-  //     userInfo.firstName = validation.checkString(
-  //       userInfo.firstName,
-  //       'first name'
-  //     );
+  async updateUserPatch(id, userInfo) {
+    id = validation.checkId(id);
+    if (userInfo.firstName)
+      userInfo.firstName = validation.checkString(
+        userInfo.firstName,
+        'first name'
+      );
 
-  //   if (userInfo.lastName)
-  //     userInfo.lastName = validation.checkString(
-  //       userInfo.lastName,
-  //       'last name'
-  //     );
-  //   const userCollection = await users();
-  //   const updateInfo = await userCollection.findOneAndUpdate(
-  //     {_id: new ObjectId(id)},
-  //     {$set: userInfo},
-  //     {returnDocument: 'after'}
-  //   );
-  //   if (!updateInfo)
-  //     throw `Error: Update failed, could not find a user with id of ${id}`;
+    if (userInfo.lastName)
+      userInfo.lastName = validation.checkString(
+        userInfo.lastName,
+        'last name'
+      );
+    const userCollection = await users();
+    const updateInfo = await userCollection.findOneAndUpdate(
+      {_id: new ObjectId(id)},
+      {$set: userInfo},
+      {returnDocument: 'after'}
+    );
+    if (!updateInfo)
+      throw `Error: Update failed, could not find a user with id of ${id}`;
 
-  //   return updateInfo;
-  // }
+    return updateInfo;
+  }
 };
 
 export default exportedMethods;
