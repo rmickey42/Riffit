31830552ca0d7f3afb792a97886170ce97e1d6b8
Evan Kupsch <ievank@me.com>
2024-12-10 02:28:10 -0500

    Implemented Server

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 import express from "express";
-const app = express();
 import configRoutes from "./routes/index.js";
-
 import exphbs from "express-handlebars";
+import session from "express-session";
+
+
+
+const app = express();
+
+
+// setup handlebars
+const handlebars = exphbs.create({
+	defaultLayout: "main",
+	layoutsDir: "./views/layouts"
+});
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+
+
+
+app.use(express.json()); // json encoding for POSTs
+app.use(express.urlencoded()); // url encoding for API
+
+
+app.use(session({
+    name: "AuthenticationState",
+    secret: "AuThSeCrEt12345",
+    saveUninitialized: false,
+    resave: false
+}))
+
+configRoutes(app)
+
+app.listen(80, ()=>{
+    console.log(`Server listening on http://localhost/`)
+})
