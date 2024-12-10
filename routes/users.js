a526012fc33509021f1d153c91fd29457ea870a8
Evan Kupsch <ievank@me.com>
2024-12-10 02:27:25 -0500

    removed auth routes from user

--- routes/users.js
+++ routes/users.js
@@ -26,35 +26,6 @@ router
     });
 
 router
-    .route('/signup')
-    .get(async (req, res) => {
-        // TODO: display page
-        return res.status(404).json({error: "Not implemented"});
-    })
-    .post(async (req, res) => {
-        // TODO: hash password
-        let username = validation.checkString(req.body.username, "username");
-        let password = validation.checkString(req.body.password, "password");
-
-        try{
-            const user = await userData.addUser(username, password);
-            return res.json(user);
-        } catch (e) {
-            return res.status(400).json({error: e});
-        }
-    });
-
-router
-    .route('/login')
-    .get(async (req, res) => {
-        // TODO: display page
-        return res.status(404).json({error: "Not implemented"});
-    })
-    .post(async (req, res) => {
-        // TODO: login
-    });
-
-router
     .route('/:userId/comments')
     .get(async (req, res) => {
         try{
