aceecd489e0be774dabf3bae5c92fb46995dfa4a
Ryan Mickey <rmickey42@gmail.com>
2024-12-16 16:32:18 -0500

    post edit

---
+++ public/js/tag_validation.js
@@ -0,0 +1,34 @@
+// Tag input fields
+
+const MAX_TAGS = 10;
+let tagCount = parseInt($('#tagCount').val());
+
+$('#add-tag-button').click(function() {
+    tagCount++;
+    const newTagInput = `
+        <label id="tag-input-${tagCount}-label" for="tag-input-${tagCount}">Tag ${tagCount}:</label>
+        <input type="text" name="tag-input-${tagCount}" placeholder="Tag" id="tag-input-${tagCount}">
+    `;
+    $(newTagInput).insertBefore('#add-tag-button');
+    $('#remove-tag-button').attr('disabled', false);
+
+    if (tagCount >= MAX_TAGS) {
+        $('#add-tag-button').attr('disabled', true);
+    }
+    $('#tagCount').val(tagCount);
+});
+
+$('#remove-tag-button').click(function() {
+    if (tagCount > 1) {
+        $(`#tag-input-${tagCount}`).remove();
+        $(`#tag-input-${tagCount}-label`).remove();
+        tagCount--;
+        $('#add-tag-button').attr('disabled', false);
+
+        if (tagCount === 1) {
+            $('#remove-tag-button').attr('disabled', true);
+        }
+    }
+    $('#tagCount').val(tagCount);
+});
+
